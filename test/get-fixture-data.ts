5384c114a3becef1f01e568782020add30c27d16
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2024-02-07 12:07:05 -0800

    tmp: switch to aegir/fixtures loader

--- test/get-fixture-data.ts
+++ test/get-fixture-data.ts
@@ -1,20 +1,23 @@
-async function getFixtureData (filename: string): Promise<Response> {
-  const fixtureDataResp = await fetch(`${process.env.FIXTURE_DATA_SERVER}/${filename}`, { method: 'GET' })
+import loadFixtures from 'aegir/fixtures'
 
-  if (!fixtureDataResp.ok) throw new Error(`Failed to fetch ${filename}: ${fixtureDataResp.statusText}`)
-  if (fixtureDataResp.body == null) throw new Error(`Failed to fetch ${filename}: no body`)
+// async function getFixtureData (filename: string): Promise<Response> {
+//   const fixtureDataResp = await fetch(`${process.env.FIXTURE_DATA_SERVER}/${filename}`, { method: 'GET' })
 
-  return fixtureDataResp
-}
+//   if (!fixtureDataResp.ok) throw new Error(`Failed to fetch ${filename}: ${fixtureDataResp.statusText}`)
+//   if (fixtureDataResp.body == null) throw new Error(`Failed to fetch ${filename}: no body`)
+
+//   return fixtureDataResp
+// }
 
 export async function getFixtureDataUint8Array (filename: string): Promise<Uint8Array> {
-  const fixtureDataResp = await getFixtureData(filename)
+  // const fixtureDataResp = await getFixtureData(filename)
 
-  return new Uint8Array(await fixtureDataResp.arrayBuffer())
+  // return new Uint8Array(await fixtureDataResp.arrayBuffer())
+  return loadFixtures(`fixture/${filename}`)
 }
 
 export async function getFixtureDataBlob (filename: string): Promise<Blob> {
-  const fixtureDataResp = await getFixtureData(filename)
-
-  return fixtureDataResp.blob()
+  // const fixtureDataResp = await getFixtureData(filename)
+  const fixtureData = loadFixtures(`fixture/${filename}`)
+  return new Blob([fixtureData])
 }
